4be9caa004c8e330f00f9d645119f218555620e2
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-05-05 21:48:00 -0500

    Anagrams Solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,50 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+/**
+ * We compare the cleaned strings of stringA and stringB to see if they are equal
+ * @param stringA - "rail safety"
+ * @param stringB - "cinema"
+ * @returns A boolean value.
+ */
+function anagrams(stringA, stringB) {
+  return cleanStr(stringA) === cleanStr(stringB);
+}
+
+/**
+ * It takes a string, removes all non-alphanumeric characters, converts it to lowercase, splits it into
+ * an array, sorts the array, and joins it back into a string
+ * @param {string} string - the string to be cleaned
+ * @returns A string with all non-alphanumeric characters removed, converted to lowercase, sorted
+ * alphabetically, and joined together.
+ */
+function cleanStr(string) {
+  return string
+    .replace(/[^\w]/g)
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+  // const charsA = {};
+  // const charsB = {};
+
+  // const stringAR = stringA.replace(/[^\w]/g, '');
+  // const stringBR = stringB.replace(/[^\w]/g, '');
+
+  // for (const char of stringAR) {
+  //   charsA[char] = charsA[char] + 1 || 1;
+  // }
+
+  // for (const char of stringBR) {
+  //   charsB[char] = charsB[char] + 1 || 1;
+  // }
+
+  // return Object.values(charsA)
+  //   .sort()
+  //   .every((v, i) => v === Object.values(charsB).sort()[i]);
+// }

69fe387278902d0164f57c19c218a93b66544e16
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-05-06 19:35:28 -0500

    Sentence Capitalization Solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,36 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const words = [];
+
+  for (const word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1, str.length));
+  }
+
+  return words.join(' ');
+}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+
+//   return result;
+// }
+
+// function capitalize(str = '') {
+//   return str.split(' ').map(capitalizeFirst).join(' ');
+// }
+
+// function capitalizeFirst(str) {
+//   return str[0].toUpperCase() + str.slice(1, str.length);
+// }
